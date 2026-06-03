26d3a724d1914fbae5a351f2b997b8441e9094ca
Vrutik Sidapara <work.vrutik.s@gmail.com>
2026-06-03 16:38:11 +0530

    Configure TiDB database connection

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,14 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
-
